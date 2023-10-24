df909128899aeacd692d3ffb4e767d92099f7b58
E-T-K <e-t-k@users.noreply.github.com>
2023-10-24 12:59:19 -0700

    pancancer/pandisease pathway results
    - update convert_outlier_lists_for_msigdb.py to be compendium-aware
    - update README with details for this process
    - add all generated files except for the pathway results

--- standalone_msigdb/source/convert_outlier_lists_for_msigdb.py
+++ standalone_msigdb/source/convert_outlier_lists_for_msigdb.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python3
 import sys
 import csv
+import os
 
 # Inputs:
 
 # INPUT-FILE: A "tall" file that contains either pancancer or pandisease results. 
-# The header row must include "sample_id" and "gene" or "Gene" colums.
+# The header row must include "sample_id", "gene" or "Gene", and "tertiary_path" columns.
 # Each subsequent row represents one gene that is an up-outlier for that sample vs PC or PD.
 # Each sample must appear vs only one CARE version and compendium (not enforced!)
 # Example input file: pan_cancer_outliers_from_PathsToOriginalTertiary.2022_01_18.tsv.tsv
 
-# COHORT-NAME: the name of the cohort. Either pan-cancer or pan-disease (not enforced)
+# COHORT-NAME: the name of the cohort. Either "pancancer" or "pandisease" (not enforced)
 
 # Output:
 # Emits rows to standard out that can go into a "wide" geneLists.txt file suitable for MSIGDB processing.
-# Each row is tab-separated. Starts with SampleID-CohortName and then lists all genes that are outliers for that
+# Each row is tab-separated. Starts with SampleID-Compendium-CohortName and then lists all genes that are outliers for that
 # sample and cohort
 
 def convert_file(filename, cohortname):
 
     seen_sample_ids = set()
     current_sample_id = ""
+    current_compendium = ""
     current_genes = []
 
     with open(filename, "r") as f:
@@ -40,18 +42,31 @@ def convert_file(filename, cohortname):
                     exit()
                 # Emit the previous sample (if there is one) and set up the new sample
                 if current_sample_id:
-                    print_row(current_sample_id, cohortname, current_genes)
+                    print_row(current_sample_id, current_compendium, cohortname, current_genes)
                 current_sample_id = row["sample_id"]
                 current_genes = [row["gene"]] 
-    
-            else: # Another gene from current sample - accumulate it
+                current_compendium = get_compendium(row["tertiary_path"])
+
+            # Another gene from current sample. Ensure we haven't suddenly switched compendia.
+            elif get_compendium(row["tertiary_path"]) != current_compendium:
+                print("\nError! Compendia must remain consistent per sample. Quitting.")
+                exit() 
+            else: # Accumulate the next gene 
                 current_genes.append(row["gene"])
 
         # Finished the file -- print the final sample
-        print_row(current_sample_id,cohortname, current_genes)
+        print_row(current_sample_id, current_compendium, cohortname, current_genes)
 
-def print_row(sampleid, cohortname, genelist):
-   print("\t".join(["{}-{}".format(sampleid, cohortname)] + genelist)) 
+# Convert eg /private/groups/treehouse/.../treehouse-care-.../compendium-v7_polya
+# to"v7"
+def get_compendium(path):
+    longname = os.path.basename(path)
+    shortname = longname.removeprefix("compendium-")
+    number=shortname.split("_")[0]
+    return number
+
+def print_row(sampleid, compendium, cohortname, genelist):
+   print("\t".join(["{}-{}-{}".format(sampleid, compendium, cohortname)] + genelist)) 
 
 
 if __name__ == "__main__":
